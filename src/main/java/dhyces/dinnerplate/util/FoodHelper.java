bc13fe5adf3d8f502e133966d9d44a1d24cc4658
dhyces <pokmon987@gmail.com>
2022-02-26 19:17:14 -0500

    Update FoodHelper.java
    Moved eating to the IBitable interface. This class is now only responsible for adding and combining effects, which may also be moved to the IBitable interface

--- src/main/java/dhyces/dinnerplate/util/FoodHelper.java
+++ src/main/java/dhyces/dinnerplate/util/FoodHelper.java
@@ -17,17 +17,6 @@ import net.minecraft.world.level.gameevent.GameEvent;
 
 public class FoodHelper {
 
-	public static void playerStaticEatBite(Level level, Player player, ItemStack stack, IBite bite) {
-		player.getFoodData().eat(bite.getNutrition(), bite.getSaturationModifier());
-		level.gameEvent(player, GameEvent.EAT, player.eyeBlockPosition());
-		level.playSound((Player)null, player.getX(), player.getY(), player.getZ(), player.getEatingSound(stack), SoundSource.NEUTRAL, 1.0F, 1.0F + (level.random.nextFloat() - level.random.nextFloat()) * 0.4F);
-		for(Pair<MobEffectInstance, Float> pair : bite.getEffects()) {
-			if (!level.isClientSide && pair.getFirst() != null && level.random.nextFloat() < pair.getSecond()) {
-				addEffect(player, new MobEffectInstance(pair.getFirst()));
-			}
-		}
-	}
-
 	public static void addEffect(LivingEntity entity, MobEffectInstance instance) {
 		var activeEffect = entity.getActiveEffectsMap().get(instance.getEffect());
 		if (activeEffect != null)
@@ -40,14 +29,5 @@ public class FoodHelper {
 			return new MobEffectInstance(instance.getEffect(), instance.getDuration() + otherInstance.getDuration(), instance.getAmplifier());
 		}
 		return instance;
-	}
-
-	/** This doesn't actually do anything with replenishing hunger, saturation, or effects, it just plays the sound, awards a stat for the item, and triggers criteria*/
-	public static void playerStaticEat(Level level, Player player, ItemStack stack) {
-		player.awardStat(Stats.ITEM_USED.get(stack.getItem()));
-		level.playSound((Player)null, player.getX(), player.getY(), player.getZ(), SoundEvents.PLAYER_BURP, SoundSource.PLAYERS, 0.5F, level.random.nextFloat() * 0.1F + 0.9F);
-		if (player instanceof ServerPlayer serverPlayer) {
-			CriteriaTriggers.CONSUME_ITEM.trigger(serverPlayer, stack);
-		}
 	}
 }
