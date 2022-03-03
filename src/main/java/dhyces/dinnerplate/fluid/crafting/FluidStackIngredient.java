6d988c3672399dcbb90d60a937dc6d9a6ca136a1
dhyces <pokmon987@gmail.com>
2022-03-02 23:41:56 -0500

    Base work on the FluidStackIngredient

--- src/main/java/dhyces/dinnerplate/fluid/crafting/FluidStackIngredient.java
+++ src/main/java/dhyces/dinnerplate/fluid/crafting/FluidStackIngredient.java
@@ -3,11 +3,22 @@ package dhyces.dinnerplate.fluid.crafting;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 
+import net.minecraft.core.Registry;
+import net.minecraft.nbt.TagParser;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.GsonHelper;
+import net.minecraftforge.common.crafting.NBTIngredient;
 import net.minecraftforge.fluids.FluidStack;
 
 public class FluidStackIngredient implements Predicate<FluidStack> {
+
+	public static final FluidStackIngredient EMPTY = new FluidStackIngredient();
 
 	private FluidStack[] stacks;
 
@@ -19,10 +30,40 @@ public class FluidStackIngredient implements Predicate<FluidStack> {
 	}
 
 	public static JsonElement toJson(FluidStackIngredient ing) {
-		return null;
+		var jsonArray = new JsonArray(ing.stacks.length);
+		for (FluidStack stack : ing.stacks) {
+			var jsonOBJ = new JsonObject();
+			jsonOBJ.addProperty("Fluid", stack.getFluid().getRegistryName().toString());
+			jsonOBJ.addProperty("Amount", stack.getAmount());
+			if (stack.hasTag())
+				jsonOBJ.add("nbt", JsonParser.parseString(stack.getTag().getAsString()));
+			jsonArray.add(jsonOBJ);
+		}
+		return jsonArray;
 	}
 
 	public static FluidStackIngredient fromJson(JsonElement e) {
-		return null;
+		if (e instanceof JsonArray arr) {
+			var ing = new FluidStackIngredient();
+			ing.stacks = new FluidStack[arr.size()];
+			for (int i = 0; i < arr.size(); i++) {
+				var obj = arr.get(i).getAsJsonObject();
+				var fluid = Registry.FLUID.get(ResourceLocation.tryParse(obj.get("Fluid").getAsString()));
+				var amount = obj.get("Amount").getAsInt();
+				var nbtElement = obj.get("nbt");
+				if (nbtElement != null) {
+					try {
+						var nbt = TagParser.parseTag(GsonHelper.getAsString(obj, "nbt"));
+						ing.stacks[i] = new FluidStack(fluid, amount, nbt);
+					} catch (CommandSyntaxException e1) {
+						e1.printStackTrace();
+					}
+				} else {
+					ing.stacks[i] = new FluidStack(fluid, amount);
+				}
+			}
+			return ing;
+		}
+		return EMPTY;
 	}
 }
