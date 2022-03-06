1d9ccbf761d235a70a4d3f423559f44d948b956d
dhyces <pokmon987@gmail.com>
2022-03-06 10:53:18 -0500

    Updated import packages

--- src/main/java/dhyces/dinnerplate/blockentity/api/IFluidWorkstation.java
+++ src/main/java/dhyces/dinnerplate/blockentity/api/IFluidWorkstation.java
@@ -1,5 +1,7 @@
 package dhyces.dinnerplate.blockentity.api;
 
+import dhyces.dinnerplate.inventory.api.IFluidHolder;
+
 // TODO: this interface will provide implementations with methods to help with recipes and other interactions between fluids
 public interface IFluidWorkstation extends IFluidHolder {
 
--- src/main/java/dhyces/dinnerplate/blockentity/api/IItemWorkstation.java
+++ src/main/java/dhyces/dinnerplate/blockentity/api/IItemWorkstation.java
@@ -1,5 +1,7 @@
 package dhyces.dinnerplate.blockentity.api;
 
+import dhyces.dinnerplate.inventory.api.IItemHolder;
+
 // TODO: this interface will provide implementations with methods to help with recipes and other interactions between items
 public interface IItemWorkstation extends IItemHolder {
 
