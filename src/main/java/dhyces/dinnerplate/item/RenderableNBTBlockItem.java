bc593b5bbf3029b782194bdd7283d66219dccca7
dhyces <pokmon987@gmail.com>
2022-03-11 12:42:58 -0500

    RenderableNBTBlockItem now has a capability function to simplify item creation, as all item forms of the blocks should have capabilities
    Must return new every time

--- src/main/java/dhyces/dinnerplate/item/RenderableNBTBlockItem.java
+++ src/main/java/dhyces/dinnerplate/item/RenderableNBTBlockItem.java
@@ -1,18 +1,37 @@
 package dhyces.dinnerplate.item;
 
+import java.util.function.BiFunction;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+import javax.annotation.Nullable;
 
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.client.IItemRenderProperties;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
 
 public class RenderableNBTBlockItem extends NBTBlockItem {
 
 	private final BlockEntityWithoutLevelRenderer renderer;
+	protected final BiFunction<ItemStack, CompoundTag, ICapabilityProvider> cap;
 	
 	public RenderableNBTBlockItem(BlockEntityWithoutLevelRenderer renderer, Block pBlock, Properties pProperties) {
+		this((i, c) -> null, renderer, pBlock, pProperties);
+	}
+	
+	public RenderableNBTBlockItem(BiFunction<ItemStack, CompoundTag, ICapabilityProvider> capability, BlockEntityWithoutLevelRenderer renderer, Block pBlock, Properties pProperties) {
 		super(pBlock, pProperties);
 		this.renderer = renderer;
+		this.cap = capability;
+	}
+	
+	@Nullable
+	@Override
+	public ICapabilityProvider initCapabilities(ItemStack stack, CompoundTag nbt) {
+		return cap.apply(stack, nbt);
 	}
 
 	@Override
