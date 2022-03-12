0bcc335254c0fe83dbfb98a33e6739a13910ebfe
dhyces <pokmon987@gmail.com>
2022-03-12 02:43:53 -0500

    Second iteration of MixingBowlItemRenderer
    Fluids now render correctly, items are yet to be accomplished

---
+++ src/main/java/dhyces/dinnerplate/capability/mixed/MixedCapability.java
@@ -0,0 +1,187 @@
+package dhyces.dinnerplate.capability.mixed;
+
+import java.util.Optional;
+
+import dhyces.dinnerplate.inventory.MixedInventory;
+import dhyces.dinnerplate.inventory.api.IMixedInventory;
+import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.common.capabilities.ICapabilitySerializable;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
+import net.minecraftforge.fluids.capability.IFluidHandler;
+import net.minecraftforge.fluids.capability.IFluidHandlerItem;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
+
+public class MixedCapability implements ICapabilitySerializable<CompoundTag>, IItemHandler, IFluidHandler {
+
+	IMixedInventory inv;
+	ItemStack container;
+	
+	public MixedCapability(IMixedInventory mixedInv) {
+		this.inv = mixedInv;
+	}
+
+	@Override
+	public int getSlots() {
+		return inv.getItemSize();
+	}
+
+	@Override
+	public ItemStack getStackInSlot(int slot) {
+		return inv.getItem(slot);
+	}
+
+	@Override
+	public ItemStack insertItem(int slot, ItemStack stack, boolean simulate) {
+		if (slot < 0 || slot >= getSlots())
+			return stack;
+		if (simulate)
+			return stack.copy().split(inv.remaining());
+		return inv.insertItemAt(stack, slot);
+	}
+
+	@Override
+	public ItemStack extractItem(int slot, int amount, boolean simulate) {
+		if (slot < 0 || slot >= getSlots())
+			return ItemStack.EMPTY;
+		if (simulate)
+			return getStackInSlot(slot).copy().split(amount);
+		return inv.removeItem(slot, amount);
+	}
+
+	@Override
+	public int getSlotLimit(int slot) {
+		return 1;
+	}
+
+	@Override
+	public boolean isItemValid(int slot, ItemStack stack) {
+		return true;
+	}
+	
+	@Override
+	public int getTanks() {
+		return inv.getFluidSize();
+	}
+
+	@Override
+	public FluidStack getFluidInTank(int tank) {
+		return inv.getFluidStack(tank);
+	}
+
+	@Override
+	public int getTankCapacity(int tank) {
+		return 100;
+	}
+
+	@Override
+	public boolean isFluidValid(int tank, FluidStack stack) {
+		return true;
+	}
+
+	@Override
+	public int fill(FluidStack resource, FluidAction action) {
+		if (resource.isEmpty())
+			return 0;
+		var maxFill = Math.min(inv.remaining() * 100, resource.getAmount());
+		if (action.execute() && maxFill > 0) {
+			inv.insertFluid(resource);
+		}
+		return maxFill;
+	}
+
+	@Override
+	public FluidStack drain(FluidStack resource, FluidAction action) {
+		if (resource.isEmpty())
+			return resource;
+		int i = 0;
+		Optional<FluidStack> stack = Optional.empty();
+		while (i < getTanks()) {
+			var next = getFluidInTank(i);
+			if (next.isFluidEqual(resource)) {
+				stack = Optional.of(next);
+				break;
+			}
+			i++;
+		}
+		var ret = stack.orElse(FluidStack.EMPTY);
+		var maxDrain = Math.min(ret.getAmount(), resource.getAmount());
+		if (action.execute()) {
+			if (maxDrain == ret.getAmount())
+				ret = inv.removeFluid(i);
+			else
+				ret.setAmount(maxDrain);
+		} else {
+			ret = ret.copy();
+			ret.setAmount(maxDrain);
+		}
+		return ret;
+	}
+
+	@Override
+	public FluidStack drain(int maxDrain, FluidAction action) {
+		if (maxDrain == 0)
+			return FluidStack.EMPTY;
+		var stack = inv.getLastFluid();
+		if (stack.isEmpty())
+			return FluidStack.EMPTY;
+		var trueMax = Math.min(stack.getAmount(), maxDrain);
+		if (action.execute()) {
+			if (trueMax == stack.getAmount())
+				stack = inv.removeLastFluid(trueMax);
+			else
+				stack.setAmount(trueMax);
+		} else {
+			stack = stack.copy();
+			stack.setAmount(trueMax);
+		}
+		return stack;
+	}
+	
+	@Override
+	public CompoundTag serializeNBT() {
+		if (inv instanceof MixedInventory impl)
+			return impl.serializeNBT();
+		return new CompoundTag();
+	}
+
+	@Override
+	public void deserializeNBT(CompoundTag nbt) {
+		if (inv instanceof MixedInventory impl)
+			impl.deserializeNBT(nbt);
+	}
+
+	@Override
+	public <T> LazyOptional<T> getCapability(Capability<T> cap, Direction side) {
+		var lazy = CapabilityItemHandler.ITEM_HANDLER_CAPABILITY.orEmpty(cap, LazyOptional.of(() -> this));
+		if (lazy.isPresent())
+			return lazy;
+		return CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY.orEmpty(cap, LazyOptional.of(() -> this));
+	}
+	
+	public static class Item extends MixedCapability implements IFluidHandlerItem {
+		
+		public Item(ItemStack container, IMixedInventory mixedInv) {
+			super(mixedInv);
+			this.container = container;
+			deserializeNBT(this.container.getOrCreateTag());
+		}
+
+		@Override
+		public ItemStack getContainer() {
+			return this.container;
+		}
+		
+		@Override
+		public <T> LazyOptional<T> getCapability(Capability<T> cap, Direction side) {
+			var lazy = CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY.orEmpty(cap, LazyOptional.of(() -> this));
+			return lazy.isPresent() ? lazy : super.getCapability(cap, side);
+		}
+	}
+}
--- src/main/java/dhyces/dinnerplate/render/item/MixingBowlItemRenderer.java
+++ src/main/java/dhyces/dinnerplate/render/item/MixingBowlItemRenderer.java
@@ -1,5 +1,10 @@
 package dhyces.dinnerplate.render.item;
 
+import org.lwjgl.opengl.GL20;
+
+import com.mojang.blaze3d.platform.GlStateManager.DestFactor;
+import com.mojang.blaze3d.platform.GlStateManager.SourceFactor;
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 
 import dhyces.dinnerplate.render.util.IFluidRenderer;
@@ -7,10 +12,13 @@ import dhyces.dinnerplate.render.util.RectPrism;
 import dhyces.dinnerplate.render.util.RenderTypes;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.block.model.ItemTransforms.TransformType;
+import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.fluids.FluidUtil;
+import net.minecraftforge.fluids.capability.IFluidHandler.FluidAction;
 
 public class MixingBowlItemRenderer extends SimpleItemRenderer implements IFluidRenderer {
 
@@ -20,10 +28,13 @@ public class MixingBowlItemRenderer extends SimpleItemRenderer implements IFluid
 		pPoseStack.pushPose();
 		renderItem(pStack, pPoseStack, pBuffer.getBuffer(ItemBlockRenderTypes.getRenderType(pStack, true)), pPackedLight, pPackedOverlay);
 		var cap = FluidUtil.getFluidHandler(pStack);
-		if (cap.isPresent())
-			tessalateItemFluids(fluidArray(cap.resolve().get().getFluidInTank(0)),
-					RectPrism.fromPixel(3, 2, 3).toPixel(13, 2.1 + cap.resolve().get().getTanks() * 0.4375f, 13),
-					pBuffer.getBuffer(RenderTypes.getFluid()), pPoseStack, pPackedOverlay, pPackedLight, Direction.UP);
+		if (cap.isPresent()) {
+			var fluidHandler = cap.resolve().get();
+			System.out.println(fluidHandler.getTanks());
+			tessalateItemFluids(fluidArray(fluidHandler.getFluidInTank(0)),
+					RectPrism.fromPixel(3, 2, 3).toPixel(13, 2.1 + (fluidHandler.getTanks() * 0.4375f), 13),
+					pBuffer.getBuffer(RenderType.entityTranslucentCull(atlas())), pPoseStack, pPackedOverlay, pPackedLight, Direction.UP);
+		}
 		pPoseStack.popPose();
 	}
 }
