db16a7e20c900a7af0ca2254704389641cc4e438
dhyces <pokmon987@gmail.com>
2022-03-16 12:38:59 -0400

    Update MeasuringCupItem.java
    -Marked for removal

--- src/main/java/dhyces/dinnerplate/item/MeasuringCupItem.java
+++ src/main/java/dhyces/dinnerplate/item/MeasuringCupItem.java
@@ -1,23 +1,14 @@
 package dhyces.dinnerplate.item;
 
 import dhyces.dinnerplate.block.api.IForkedInteractAdapter;
-import dhyces.dinnerplate.capability.fluid.MeasuredFluidCapability;
 import dhyces.dinnerplate.registry.BlockRegistry;
-import dhyces.dinnerplate.render.item.MeasuringCupItemRenderer;
-import dhyces.dinnerplate.util.FluidHelper;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.world.item.ItemStack;
+import dhyces.dinnerplate.client.render.item.MeasuringCupItemRenderer;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraftforge.common.capabilities.ICapabilityProvider;
 
+@Deprecated(forRemoval = true) /** Marked for removal due to not having any changes in logic from the superclass*/
 public class MeasuringCupItem extends RenderableNBTBlockItem implements IForkedInteractAdapter<BlockEntity> {
 
 	public MeasuringCupItem(Properties pProperties) {
 		super(new MeasuringCupItemRenderer(), BlockRegistry.MEASURING_CUP_BLOCK.get(), pProperties);
-	}
-	
-	@Override
-	public ICapabilityProvider initCapabilities(ItemStack stack, CompoundTag nbt) {
-		return new MeasuredFluidCapability(stack, 1000, FluidHelper.PILE);
 	}
 }
