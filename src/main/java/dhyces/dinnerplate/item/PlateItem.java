84ffaa226d1e239fc6b3a6c41a74ce7454be5f90
dhyces <pokmon987@gmail.com>
2022-05-13 20:47:43 -0400

    Change to the new updated classes

--- src/main/java/dhyces/dinnerplate/item/PlateItem.java
+++ src/main/java/dhyces/dinnerplate/item/PlateItem.java
@@ -2,16 +2,20 @@ package dhyces.dinnerplate.item;
 
 import dhyces.dinnerplate.client.render.item.PlateItemRenderer;
 import dhyces.dinnerplate.sound.DinnerSoundTypes;
+import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.core.Direction;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.level.block.Block;
+import net.minecraftforge.client.IItemRenderProperties;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.ItemStackHandler;
 
-public class PlateItem extends RenderableNBTBlockItem {
+import java.util.function.Consumer;
+
+public class PlateItem extends CapabilityNBTBlockItem {
 
 	public PlateItem(Block block, Properties pProperties) {
 		super((stack, tag) -> {
@@ -20,7 +24,17 @@ public class PlateItem extends RenderableNBTBlockItem {
 				public <T> LazyOptional<T> getCapability(Capability<T> cap, Direction side) {
 					return CapabilityItemHandler.ITEM_HANDLER_CAPABILITY.orEmpty(cap, LazyOptional.of(() -> new ItemStackHandler(1)));
 				}};
-		}, new PlateItemRenderer(), block, pProperties);
+		}, block, pProperties);
+	}
+
+	@Override
+	public void initializeClient(Consumer<IItemRenderProperties> consumer) {
+		consumer.accept(new IItemRenderProperties() {
+			@Override
+			public BlockEntityWithoutLevelRenderer getItemStackRenderer() {
+				return new PlateItemRenderer();
+			}
+		});
 	}
 
 	@Override
