7b32145ad470569e4171477fb0ee0552a8333a9f
dhyces <pokmon987@gmail.com>
2022-05-23 12:47:01 -0400

    Added safe methods, a getAmount method, and changed to be a record

--- src/main/java/dhyces/dinnerplate/util/FlutemStack.java
+++ src/main/java/dhyces/dinnerplate/util/FlutemStack.java
@@ -4,9 +4,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
 
 // Obj wrapper for either a fluid or an item
-public class FlutemStack {
-
-    private final Object o;
+public record FlutemStack(Object o) {
 
     public FlutemStack(Object o) {
         this.o = o;
@@ -23,20 +21,32 @@ public class FlutemStack {
         return isItem() ? getItemStack().sameItem((ItemStack) oIn) : getFluidStack().isFluidEqual((FluidStack) oIn);
     }
 
+    public int getAmount() {
+        return isItem() ? getItemStack().getCount() : getFluidStack().getAmount();
+    }
+
     public boolean isEmpty() {
         return isItem() ? getItemStack().isEmpty() : getFluidStack().isEmpty();
     }
 
-    public ItemStack getItemStack() {
-        if (isItem())
-            return (ItemStack) o;
+    public ItemStack getItemStackSafe() {
+        if (o instanceof ItemStack stack)
+            return stack;
         return ItemStack.EMPTY;
     }
 
-    public FluidStack getFluidStack() {
-        if (isFluid())
-            return (FluidStack) o;
+    public FluidStack getFluidStackSafe() {
+        if (o instanceof FluidStack stack)
+            return stack;
         return FluidStack.EMPTY;
+    }
+
+    public ItemStack getItemStack() {
+        return (ItemStack) o;
+    }
+
+    public FluidStack getFluidStack() {
+        return (FluidStack) o;
     }
 
     public boolean isItem() {
