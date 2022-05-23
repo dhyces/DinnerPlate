f7de423ee63e250f7b8d81925378c43018d19d1d
dhyces <pokmon987@gmail.com>
2022-05-23 12:54:29 -0400

    Changed names for bitables

--- src/main/java/dhyces/dinnerplate/client/ClientInit.java
+++ src/main/java/dhyces/dinnerplate/client/ClientInit.java
@@ -94,7 +94,7 @@ public class ClientInit {
     private void prepareSeparateModels() {
         edibleItems.stream().filter(c -> !(c instanceof IBitable))
                 .map(c -> {
-                    var n = new ResourceLocation(MODID, "item/bitten/bitten_" + c.getRegistryName().getPath());
+                    var n = new ResourceLocation(MODID, "item/bitten/" + c.getRegistryName().getPath() + "_bitten");
                     System.out.println(c + " new one " + n);
                     return n;
                 })
