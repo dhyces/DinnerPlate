72020eb515b96ae7bac7e6e9b03bd227a75b36e4
dhyces <pokmon987@gmail.com>
2022-05-23 12:55:34 -0400

    Some more work on the basic fluid stack ingredient
    Still needs more work

--- src/main/java/dhyces/dinnerplate/fluid/crafting/FluidStackIngredient.java
+++ src/main/java/dhyces/dinnerplate/fluid/crafting/FluidStackIngredient.java
@@ -4,12 +4,21 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.core.Registry;
+import net.minecraft.nbt.NbtIo;
+import net.minecraft.nbt.NbtOps;
+import net.minecraft.nbt.NbtUtils;
 import net.minecraft.nbt.TagParser;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.util.GsonHelper;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraftforge.common.crafting.CraftingHelper;
+import net.minecraftforge.common.crafting.NBTIngredient;
 import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.function.Predicate;
 import java.util.stream.Stream;
@@ -21,11 +30,22 @@ public class FluidStackIngredient implements Predicate<FluidStack> {
     private FluidStack[] stacks;
 
     public static JsonElement toJson(FluidStackIngredient ing) {
+        if (ing.stacks == null || ing.stacks.length == 0)
+            throw new IllegalStateException("Fluid ingredient is invalid. Either stacks is null or it contains no FluidStacks");
+        if (ing.stacks.length == 1) {
+            var stack = ing.stacks[0];
+            var jsonOBJ = new JsonObject();
+            jsonOBJ.addProperty("fluid", stack.getFluid().getRegistryName().toString());
+            jsonOBJ.addProperty("amount", stack.getAmount());
+            if (stack.hasTag())
+                jsonOBJ.add("nbt", JsonParser.parseString(stack.getTag().getAsString()));
+            return jsonOBJ;
+        }
         var jsonArray = new JsonArray(ing.stacks.length);
         for (FluidStack stack : ing.stacks) {
             var jsonOBJ = new JsonObject();
-            jsonOBJ.addProperty("Fluid", stack.getFluid().getRegistryName().toString());
-            jsonOBJ.addProperty("Amount", stack.getAmount());
+            jsonOBJ.addProperty("fluid", stack.getFluid().getRegistryName().toString());
+            jsonOBJ.addProperty("amount", stack.getAmount());
             if (stack.hasTag())
                 jsonOBJ.add("nbt", JsonParser.parseString(stack.getTag().getAsString()));
             jsonArray.add(jsonOBJ);
@@ -34,28 +54,38 @@ public class FluidStackIngredient implements Predicate<FluidStack> {
     }
 
     public static FluidStackIngredient fromJson(JsonElement e) {
+        if (e instanceof JsonObject obj) {
+            var ing = new FluidStackIngredient();
+            ing.stacks = new FluidStack[1];
+            var fluid = ForgeRegistries.FLUIDS.getValue(ResourceLocation.tryParse(obj.get("fluid").getAsString()));
+            var amount = obj.get("amount").getAsInt();
+            var nbtElement = obj.get("nbt");
+            var nbt = nbtElement != null ? CraftingHelper.getNBT(nbtElement) : null;
+            ing.stacks[0] = new FluidStack(fluid, amount, nbt);
+            return ing;
+        }
         if (e instanceof JsonArray arr) {
             var ing = new FluidStackIngredient();
             ing.stacks = new FluidStack[arr.size()];
             for (int i = 0; i < arr.size(); i++) {
                 var obj = arr.get(i).getAsJsonObject();
-                var fluid = Registry.FLUID.get(ResourceLocation.tryParse(obj.get("Fluid").getAsString()));
-                var amount = obj.get("Amount").getAsInt();
+                var fluid = ForgeRegistries.FLUIDS.getValue(ResourceLocation.tryParse(obj.get("fluid").getAsString()));
+                var amount = obj.get("amount").getAsInt();
                 var nbtElement = obj.get("nbt");
-                if (nbtElement != null) {
-                    try {
-                        var nbt = TagParser.parseTag(GsonHelper.getAsString(obj, "nbt"));
-                        ing.stacks[i] = new FluidStack(fluid, amount, nbt);
-                    } catch (CommandSyntaxException e1) {
-                        e1.printStackTrace();
-                    }
-                } else {
-                    ing.stacks[i] = new FluidStack(fluid, amount);
-                }
+                var nbt = nbtElement != null ? CraftingHelper.getNBT(nbtElement) : null;
+                ing.stacks[i] = new FluidStack(fluid, amount, nbt);
             }
             return ing;
         }
         return EMPTY;
+    }
+
+    public static FluidStack fluidStackfromJson(JsonObject obj) {
+        var fluid = ForgeRegistries.FLUIDS.getValue(ResourceLocation.tryParse(obj.get("fluid").getAsString()));
+        var amount = obj.get("amount").getAsInt();
+        var nbtElement = obj.get("nbt");
+        var nbt = nbtElement != null ? CraftingHelper.getNBT(nbtElement) : null;
+        return new FluidStack(fluid, amount, nbt);
     }
 
     @Override
