f1f60e5e7cd54b611430f4accbe754726b6f4b6d
dhyces <pokmon987@gmail.com>
2022-06-21 17:11:47 -0400

    Change to use EntityBlock interface

--- src/main/java/dhyces/dinnerplate/block/api/AbstractDinnerBlock.java
+++ src/main/java/dhyces/dinnerplate/block/api/AbstractDinnerBlock.java
@@ -13,13 +13,14 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 
-public abstract class AbstractDinnerBlock<T extends AbstractDinnerBlockEntity> extends BaseEntityBlock implements IForkedInteract<T> {
+public abstract class AbstractDinnerBlock<T extends AbstractDinnerBlockEntity> extends Block implements EntityBlock, IForkedInteract<T> {
 
     protected AbstractDinnerBlock(Properties properties) {
         super(properties);
